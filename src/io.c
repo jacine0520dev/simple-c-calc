8abd4e9cf003e2e04501b6f92320385bfd2a7d33
Jacine0520 <jacine0520code@gmail.com>
2025-08-03 10:18:21 -0400

    Update io.c

--- src/io.c
+++ src/io.c
@@ -3,20 +3,19 @@
 #include <string.h>
 #include "io.h"
 
-//change the size of the buffer that holds user input in promptUser
-#define BUFFER_SIZE 1000
-
 /*promptUser: prompts the user for an input with a custom prompt and returns that input
 parameter: prompt (string) is just the question asking for the user input
 variable: buffer (string) is just a buffer for my input
 variable: strSize (int) is the size of the string in the buffer
 return: returns an input (string)*/
 char *promptUser(char prompt[]){
+    //setting size of the buffer
+    size_t bufSize = 1000
     //print the prompt
     printf("%s", prompt);
 
     //getting user inpuet and writing it to a buffer
-    char buffer[BUFFER_SIZE] = {0};
+    char buffer[bufSize] = {0};
     if(fgets(buffer, sizeof(buffer), stdin) == NULL){
         return NULL;
     }
