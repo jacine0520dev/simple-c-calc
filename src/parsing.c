c300bb9baf7a693921d3d2c27829317b3fac6580
Jacine0520 <jacine0520code@gmail.com>
2025-08-03 04:38:30 -0400

    Update parsing.c

--- src/parsing.c
+++ src/parsing.c
@@ -4,50 +4,52 @@
 #include <string.h>
 #include "parsing.h"
 
-char **parseOpp(char *opp){
-    char **oppArr = NULL;
+char **parseOpe(char *ope){
     char nums[11] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'};
-    char opps[4] = {'+', '-', '*', '/'};
+    char opes[4] = {'+', '-', '*', '/'};
+    char **opeArr = NULL;
+    size_t opeSize = strlen(ope);
+    size_t tokkenCount = 0;
     size_t bufSize = 100;
     size_t bufCount = 0;
-    size_t parseCount = 0;
-    size_t oppSize = strlen(opp);
 
-    //loop through the opperation
-    for(size_t i = 0; i <= oppSize; i++){
+    //loop through the operation
+    for(size_t i = 0; i <= opeSize; i++){
+        //create a buffer
         char *buf = malloc(bufSize * sizeof(char));
 
-        //check for nums
+        //check for number characters
         for(size_t j = 0; j <= strlen(nums); j++){
-            if(opp[i] == nums[j]){
-                buf[bufCount] = opp[i];
+            //if there are fill the first index of the buffer
+            if(ope[i] == nums[j]){
+                buf[bufCount] = ope[i];
                 bufCount++;
                 break;
             }
-            //if the first char of an operation is not a number return null and print an error message
-            else if(i == 0 && opp[i] != nums[j] && j >= strlen(nums)){
-                printf("ERROR! An opperation needs to start with a \"number\"!");
+            //if the first character of an operation is not a number return null and print an error message
+            else if(i == 0 && ope[i] != nums[j] && j >= strlen(nums)){
+                printf("ERROR! An opeeration needs to start with a \"number\"!");
                 return NULL;
             }
         }
-        //check for opps
-        for(size_t j = 0; j <= strlen(opps); j++){
-            //if there is an opperator end the first parse and add a parse for the opperator
-            if(opp[i] == opps[j]){
-                oppArr = realloc(oppArr, parseCount * sizeof(char*));
-                oppArr[parseCount] = malloc(strlen(buf));
-                for(size_t k = 0; k <= strlen(oppArr[parseCount]); k++){
-                    oppArr[parseCount][k] = buf[k];
+        //check for operstors
+        for(size_t j = 0; j <= strlen(opes); j++){
+            //if there is an operator end the number tokken and add a tokken for the opeerator
+            if(ope[i] == opes[j]){
+                opeArr = realloc(opeArr, tokkenCount * sizeof(char*));
+                opeArr[tokkenCount] = malloc(strlen(buf));
+                for(size_t k = 0; k <= strlen(opeArr[tokkenCount]); k++){
+                    opeArr[tokkenCount][k] = buf[k];
                 }
-                parseCount++;
-                oppArr = realloc(oppArr, parseCount * sizeof(char*));
-                oppArr[parseCount] = malloc(sizeof(opps[j]));
-                oppArr[parseCount][0] = opps[j];
-                parseCount++;
+                tokkenCount++;
+                opeArr = realloc(opeArr, tokkenCount * sizeof(char*));
+                opeArr[tokkenCount] = malloc(sizeof(opes[j]));
+                opeArr[tokkenCount][0] = opes[j];
+                tokkenCount++;
                 break;
             }
         }
     }
 
-    return oppArr;
+    return opeArr;
 }
